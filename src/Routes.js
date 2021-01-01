5f874b56b8f83b16b09d59cf70700fa0cf54d2dc
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-12-31 22:37:00 -0300

    Initial commit.

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
